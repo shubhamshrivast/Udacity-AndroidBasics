17b7d740070fad6acd92da0fa27c4d8bbe3fd205
Shubham Shrivastava <shubham.shrivast07@gmail.com>
2018-06-12 23:17:26 +0530

    Images added in lists

--- MiwokApp/app/src/main/java/com/example/android/miwok/Word.java
+++ MiwokApp/app/src/main/java/com/example/android/miwok/Word.java
@@ -3,11 +3,20 @@ package com.example.android.miwok;
 public class Word {
     private String mMiwoktranslation;
     private String mDefaulttranslation;
+    private static final int NO_IMAGE_PROVIDED = -1;
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
 
-    public Word(String miwoktranslation, String defaulttranslation) {
+    public Word(String defaulttranslation, String miwoktranslation, int imageresourceid) {
+        mMiwoktranslation = miwoktranslation;
+        mDefaulttranslation = defaulttranslation;
+        mImageResourceId = imageresourceid;
+    }
+
+    public Word(String defaulttranslation, String miwoktranslation) {
         mMiwoktranslation = miwoktranslation;
         mDefaulttranslation = defaulttranslation;
     }
+
 
     public String getmiwoktranslation() {
         return mMiwoktranslation;
@@ -15,5 +24,14 @@ public class Word {
 
     public String getdefaulttranslation() {
         return mDefaulttranslation;
+    }
+
+    public int getimageResoursceid() {
+        return mImageResourceId;
+    }
+
+
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
     }
 }
--- MiwokApp/app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ MiwokApp/app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -3,18 +3,23 @@ package com.example.android.miwok;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
-    public WordAdapter(Activity context, ArrayList<Word> wordadapter) {
+    private int mColorResourceId;
+
+    public WordAdapter(Activity context, ArrayList<Word> wordadapter, int categoryColour) {
         super(context, 0, wordadapter);
+        mColorResourceId = categoryColour;
     }
 
     @Override
@@ -33,6 +38,18 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         TextView defaultTextView = (TextView) listItemView.findViewById(R.id.english);
         defaultTextView.setText(currentword.getdefaulttranslation());
+
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
+        if (currentword.hasImage()) {
+            imageView.setImageResource(currentword.getimageResoursceid());
+            imageView.setVisibility(View.VISIBLE);
+        } else {
+            imageView.setVisibility(View.GONE);
+        }
+
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        textContainer.setBackgroundColor(color);
 
         return listItemView;
     }
